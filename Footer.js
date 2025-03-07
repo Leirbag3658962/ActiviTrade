cfa016f71318ecbc4655312300c729bf0e10303a
VINCENT Gabriel <vincentgab78@gmail.com>
2025-03-07 10:20:29 +0100

    navbar & footer + style

---
+++ Footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let footer = document.getElementById("footer");
+
+    footer.innerHTML = `
+        <p>&copy; 2025 - ActiviTrade. Tous droits réservés.</p>
+        <a href="mentions-legales.html" style="color: white;">Mentions légales</a>
+    `;
+});
