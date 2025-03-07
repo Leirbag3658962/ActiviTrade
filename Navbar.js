96a64039c4d4bc3e3bebbf3c003efed3fb13b1ad
VINCENT Gabriel <vincentgab78@gmail.com>
2025-03-07 22:31:07 +0100

    logo

--- Navbar.js
+++ Navbar.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let navbar = document.getElementById("navbar");
 
     navbar.innerHTML = `
-        <p>Navbar du site ActiviTrade (à modifier)</p>
+        <ul>
+            <li><a href="">Activit&eacutes</a></li>
+            <li><a href="">Cr&eacuteation</a></li>
+            <li><a href="">Mon Compte</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><input type="text"></li>
+        </ul>
     `;
 });
