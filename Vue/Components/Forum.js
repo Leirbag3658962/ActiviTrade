ccad26c8b32bc65d2806a3f38cf6567e2eb6e952
Ye-iii <jiye63892@eleve.isep.fr>
2025-05-02 17:47:54 +0200

    Update and rename ForumAnswer.js to Forum.js

---
+++ Vue/Components/Forum.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {string} replyId - 
+ */
+function toggleReplyForm(replyId) {
+    const form = document.getElementById('reply-form-' + replyId);
+    if (form) {
+        if (form.style.display === "none" || form.style.display === "") {
+            form.style.display = "block";
+        } else {
+            form.style.display = "none";
+        }
+    } else {
+        console.error("L'élément avec l'ID reply-form-" + replyId + " n'a pas été trouvé");
+    }
+}
