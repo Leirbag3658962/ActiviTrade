eea81a267ccbf16f70cb03ac400a2fd8c86129fa
Ye-iii <jiye63892@eleve.isep.fr>
2025-05-07 22:44:57 +0200

    Update CreationForum.php

--- Vue/Pages/CreationForum.php
+++ Vue/Pages/CreationForum.php
@@ -1,6 +1,7 @@
 <?php
-
 session_start();
+$_SESSION['idUser'] = 1;
+
 require_once "../../Modele/LienPDO.php";
 $pdo = lienPDO();
 
@@ -11,17 +12,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idUser = $_SESSION['idUser']; 
     $date = date("Y-m-d H:i:s");
 
+    $idParent = NULL;  
+
     $sql = "INSERT INTO forum (theme, date, contenu, idUser, idParent) 
             VALUES (:theme, :date, :contenu, :idUser, :idParent)";
 
     $stmt = $pdo->prepare($sql);
 
     $stmt->bindParam(':theme', $theme);
-    $stmt->bindParam(':date', $date_added);
+    $stmt->bindParam(':date', $date);
     $stmt->bindParam(':contenu', $contenu);
-    $stmt->bindParam(':idUser', $user_id);
-    $idParent = NULL;
-    $stmt->bindParam(':idParent', $idParent);
+    $stmt->bindParam(':idUser', $idUser);
+    $stmt->bindParam(':idParent', $idParent);  
 
     if ($stmt->execute()) {
         echo "Le sujet a été créé avec succès.";
@@ -30,44 +32,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Style/Forum.css">
-    <link rel="stylesheet" href="../Style/Navbar.css">
-    <link rel="stylesheet" href="../Style/Footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>Créer un Forum</title>
-</head>
-<body>
-<header id="navbar" class="navbar"></header>
-
-<div class="main-content">
-    <h1>Création d'un Nouveau Sujet</h1>
-    <div class="createforum-container">
-
-        <form action="create-thread.php" method="POST">
-            <label for="title">Thème:</label>
-            <input type="text" id="title" name="title" required><br>
-
-            <label for="context">Description:</label>
-            <textarea id="context" name="context" rows="4" cols="50" required></textarea><br>
-
-            <button id="Button" type="submit">Confirmer</button>
-        </form>
-    </div>
-</div>
-
-<footer id="footer" class="footer"></footer>
-
-<script src="../Components/Navbar.js"></script>
-<script>
-    document.getElementById("navbar").innerHTML = Navbar();
-</script>
-<script src="../Components/NavbarAnim.js"></script>
-<script src="../Components/Footer.js"></script>
-</body>
-</html>
