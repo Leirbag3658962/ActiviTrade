95ddf63499ac2a0db8d9832cc720311edfeda8e4
Fant0m80 <fant.home80@gmail.com>
2025-05-09 16:29:01 +0200

    Début Création

--- Modele/LienPDO.php
+++ Modele/LienPDO.php
@@ -15,7 +15,7 @@ function lienPDO(){
     $pageActuelle = "utilisateur";
 
     try {
-        $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
 
