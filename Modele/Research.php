2f364598b5c771d6dda7db9b28ccfb11f363b18e
Fant0m80 <fant.home80@gmail.com>
2025-05-21 11:01:24 +0200

    recherchetheme

--- Modele/Research.php
+++ Modele/Research.php
@@ -29,4 +29,37 @@ function recherche($mot){
         echo "<a> Erreur BDD lors de l'affichage des mentions légales: " . htmlspecialchars($e->getMessage()) . "</a>";
    }
 }
+
+function rechercheTheme($mot){
+    $pdo = getPDO();
+    if(!$pdo){
+        echo "<a> Erreur: Connexion BDD non fournie.</a>";
+    }
+    testValidationForm($mot);
+    try{
+        $idList = array();
+
+        $sqlrecherche = "SELECT at.idActivite FROM activite as a 
+                        JOIN theme as t JOIN activite_theme as at 
+                        ON a.idActivite = at.idActivite and at.idTheme = t.idTheme
+                        WHERE t.theme LIKE :mot ORDER BY at.idActivite DESC";
+
+        $stmtrecherche = $pdo->prepare($sqlrecherche);
+        $motrecherche = "%".$mot."%";
+        $stmtrecherche->bindParam(':mot', $motrecherche);
+
+        $stmtrecherche->execute();
+
+	    
+	    if($stmtrecherche){
+		    while ($row = $stmtrecherche->fetch(PDO::FETCH_ASSOC)){
+                $idList[] = $row['idActivite'];
+                // echo "<a>".$row['idActivite']."</a>";
+		    }
+            return $idList;
+	    }
+    } catch (PDOException $e) {
+        echo "<a> Erreur BDD lors de l'affichage des mentions légales: " . htmlspecialchars($e->getMessage()) . "</a>";
+   }
+}
 ?>
